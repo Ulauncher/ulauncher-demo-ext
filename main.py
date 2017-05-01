de0040f776a319c4374230057d1b4b7cb7bf0905
Aleksandr Gornostal <sanya.gornostal@gmail.com>
2017-05-01 10:00:51 +0300

    init

---
+++ main.py
@@ -0,0 +1,43 @@
+from time import sleep
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+
+
+class DemoExtension(Extension):
+
+    def __init__(self):
+        super(DemoExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = []
+        for i in range(5):
+            data = {'new_name': 'Item %s was clicked' % i}
+            items.append(ExtensionResultItem(icon='images/icon.png',
+                                             name='Item %s' % i,
+                                             description='Item description %s' % i,
+                                             on_enter=ExtensionCustomAction(data, keep_app_open=True)))
+
+        return RenderResultListAction(items)
+
+
+class ItemEnterEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        data = event.get_data()
+        return RenderResultListAction([ExtensionResultItem(icon='images/icon.png',
+                                                           name=data['new_name'],
+                                                           on_enter=HideWindowAction())])
+
+
+if __name__ == '__main__':
+    DemoExtension().run()
